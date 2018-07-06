6a844fdaaf1b62e061f4d34ebbcfda942ddba4be
Qi Wang <katiawang@live.com>
2018-07-05 23:55:08 -0400

    00

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks {
-    public class BaseGradeBook {
+    public abstract class BaseGradeBook {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
